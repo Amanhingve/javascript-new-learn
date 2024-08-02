117b7e79c53c746f1fa6eda9cf231d2ffa90a49e
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-02 16:15:07 +0530

    Update 02_dataTypes.js

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,3 +6,27 @@ console.log (3
     + 3)// code readability should be high
 
 console.log ("aman")
+
+
+
+let name = "aman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
+// documentation : ecma262 -> https://tc39.es/ecma262/#sec-tokens
+// documentation : mdn web docs -> https://developer.mozilla.org/en-US/docs/Web/JavaScript
