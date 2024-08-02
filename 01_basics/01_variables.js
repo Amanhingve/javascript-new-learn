23d5ed5df1fbc9f7be1016fe79ba6dc5357f2b95
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-02 14:27:31 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,11 @@ acountCity = "chhinwara"
 
 // accountId = 123456 // no change constant variable
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
