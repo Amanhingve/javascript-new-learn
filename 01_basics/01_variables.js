ef3fb97df5f7006b917019ddaf367e3e9ac31210
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-02 08:30:28 +0000

    container comfig done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 123445
+let accountEmail = "kI3pD@example.com"
+var accountPassword = "32145"
+acountCity = "chhinwara"
+
+// accountId = 123456 // no change constant variable
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, acountCity]);
