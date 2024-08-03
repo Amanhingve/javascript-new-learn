ce3485f7a1e981d49aebf4471e85b253f00de16d
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-03 15:49:19 +0000

    document change

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('aman-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -15,21 +15,27 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
+// 
+// console.log(gameName.lastIndexOf('a'));
+// console.log(gameName.slice(2, 5));
+// console.log(gameName.substring(2, 5));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4) // do not use (-8, 4)
 console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   aman    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim());  //trim use dom --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://aman.com/aman%20hingve"
 
 console.log(url.replace('%20', '-'))
 
 console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); //split use dom --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+
+
