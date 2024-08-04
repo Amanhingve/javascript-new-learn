20b7c70a397a7ffabc1e872b3e6cabac71e0623e
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-04 12:10:55 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -25,6 +25,8 @@ const hundreds = 1000000
 // console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10 + 1);
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
