1a495dab7fad4ea297a3a2f1e8a3f349d224ec61
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-11 17:26:56 +0530

    Update 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,7 +26,7 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)  //----> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
