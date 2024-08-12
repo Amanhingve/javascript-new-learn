d38303af9c44b67ede9b5363044c2cab46ac0a02
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-12 10:05:06 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,12 @@
 // ++++++++++++++++++++++++++++++++++++ functions +++++++++++++++++++++++++++++++++++//
 
+//
+
 function sayMyName(){
-    console.log("H");
-    console.log("I");
-    console.log("T");
-    console.log("E");
-    console.log("S");
-    console.log("H");
+    console.log("A");
+    console.log("M");
+    console.log("A");
+    console.log("N");
 }
 
 // sayMyName()
@@ -36,8 +36,8 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-// console.log(loginUserMessage("hitesh"))
-// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("aman"))
+// console.log(loginUserMessage("aman"))
 
 
 function calculateCartPrice(val1, val2, ...num1){
@@ -47,7 +47,7 @@ function calculateCartPrice(val1, val2, ...num1){
 // console.log(calculateCartPrice(200, 400, 500, 2000))
 
 const user = {
-    username: "hitesh",
+    username: "aman",
     prices: 199
 }
 
