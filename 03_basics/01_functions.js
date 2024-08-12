70e534d9a133634856705cc58d3f991dda783a0a
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-12 10:15:42 +0530

    Update 01_functions.js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,6 +39,17 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("aman"))
 // console.log(loginUserMessage("aman"))
 
+// function calculateCartPrice(num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200))
+
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200, 400, 500))
+
 
 function calculateCartPrice(val1, val2, ...num1){
     return num1
