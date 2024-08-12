690ec7fd3c9c7ae070460d76bfcd0ea0df60fc6b
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-08-12 10:39:50 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "aman"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "aman"
+    if (username === "aman") {
         const website = " youtube"
         // console.log(username + website);
     }
