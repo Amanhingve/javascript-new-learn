d0689959aa756457dedf38673c52b777465183a1
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-09-01 14:19:53 +0000

    keydetect

--- 07_projects/02project/script.js
+++ 07_projects/02project/script.js
@@ -4,16 +4,16 @@ window.addEventListener("keydown", (e) => {
     insert.innerHTML = `
         <div class="key">
             <table>   
-            <tr>
-               <th>key</th>
-               <th>keycode</th>
-               <th>code</th> 
-            </tr>
-            <tr>
-                <td>${e.key === " " ? "space" : e.key}</td>
-                <td>${e.keyCode}</td>
-                <td>${e.code}</td>
-            </tr>
+                <tr>
+                <th>key</th>
+                <th>keycode</th>
+                <th>code</th> 
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? "space" : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
             </table>
         </div>
     `;
