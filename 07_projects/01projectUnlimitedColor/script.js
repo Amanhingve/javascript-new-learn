d51261a312c3f732c5cffaec0ef28fdfa9c3c013
Amanhingve <101005851+Amanhingve@users.noreply.github.com>
2024-09-01 13:39:51 +0000

    colorchange project

---
+++ 07_projects/01projectUnlimitedColor/script.js
@@ -0,0 +1,28 @@
+// generate random color
+
+const generateRandomColor = () => {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+let intervalId;
+const startchangeColor = () => {
+    if (!intervalId){intervalId = setInterval(changeBgColor, 1000);}
+    function changeBgColor() {
+        document.body.style.backgroundColor = generateRandomColor();
+    }
+}
+
+const stopchangeColor = () => {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.querySelector('#start').addEventListener('click', startchangeColor);
+document.querySelector('#stop').addEventListener('click', stopchangeColor);
+
+
