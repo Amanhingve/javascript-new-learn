89c33dd35cbe1cc6169dad289a2338744562f17b
Aman hingve <101005851+Amanhingve@users.noreply.github.com>
2024-09-04 20:03:03 +0530

    Update getter_setter.js

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,1 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+
+    get password(){
+        return `${this._password}aman`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const aman = new User("a@aman.ai", "abc")
+console.log(aman.email);
