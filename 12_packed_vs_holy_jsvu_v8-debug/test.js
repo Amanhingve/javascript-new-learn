b8524bd252aecb70b1b95d5d6f9e25e50ddd1b64
Aman hingve <101005851+Amanhingve@users.noreply.github.com>
2024-09-05 11:08:27 +0000

    arry use

---
+++ 12_packed_vs_holy_jsvu_v8-debug/test.js
@@ -0,0 +1,56 @@
+// const myArry = []
+// %DebugPrint(myArry) // v8-debug --> jsvu debugger 
+
+// continious, Holey
+
+// SMI (small integer)
+// Packed element
+//  Double (float, sting, function)
+
+const arrTwo = [1, 2, 3, 4, 5]
+//  PACKED_SMI_ELEMENTS
+
+arrTwo.push(6.0)
+//  PACKED_DOUBLE_ELEMENTS
+
+arrTwo.push('7')
+// PACKED_ELEMENTS
+
+arrTwo[10] = 11
+// HOLEY_ELEMENTS
+
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[9]);
+
+// bound check 
+// hasOwnProperty (arrTwo, 9)
+// hasOwnProperty(arrTwo.prototype, 10)
+// hasOwnProperty(Object-prototype, 10)
+
+// holes are very expensive in js
+
+const arrThree = [1,2,3,4,5]
+console.log(arrThree[2]);
+
+// SMI > DOUBLE > PACKED 
+// H_SMI > H_DOUBLE > H_PACKED 
+
+const arrFour = new Array(3)
+// just 3 holes. HOLEY_SMI_ELEMENTS
+arrFour [0] = '1' // HOLEY_ELEMENTS
+arrFour [1] = '2' // HOLEY_ELEMENTS
+arrFour [2] = '3' // HOLEY_ELEMENTS
+
+const arrFive = [] 
+arrFive.push('1') // PACKED_ELEMENTS 
+arrFive.push('2') // PACKED_ELEMENTS
+arrFive.push('3') // PACKED_ELEMENTS
+
+const arrSix = [1, 2, 3, 4, 5]
+arrSix.push(NaN) // PACKED_DOUBLE
+arrSix.push(Infinity) //PACKED_DOUBLE
+
+// for, for-of, forEach,  
+
+
